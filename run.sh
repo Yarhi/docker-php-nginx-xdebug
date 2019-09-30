108ef94b6f3fc7431fd01db775576ace6d6dd0c9
Gael Huot <gael.huot@livdeo.fr>
2019-09-30 08:39:32 +0200

    Inital commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# You can just do "docker-compose up" or launch this script in ROOT to append the vhost in your /etc/hosts
+
+docker-compose build
+
+vhost="127.0.0.1 php-docker.local"
+rs=$(grep -q "$vhost" /etc/hosts; echo $?)
+
+if [ $rs -eq 1 ] 
+then
+    echo $vhost >> /etc/hosts
+fi
+
+
+docker-compose up
